d1741c8ce3e467cbc5e9ccf8317b1d0e741051a8
ADHI18s <sadhi684@gmail.com>
2024-09-26 10:35:28 +0530

    changes in variable.tf pushed

--- terraform/ec2_instance/variable.tf
+++ terraform/ec2_instance/variable.tf
@@ -1,4 +1,18 @@
 variable "region" {
-  description = "region of the instance"
+  description = "region of the vpc"
   default = "ap-south-1"
+}
+variable "availability_zones" {
+  description = "availability zone of subnet public"
+}
+
+variable "ami" {
+  description = "ami for the 2 instanses"
+}
+variable "instance_type" {
+  default = "type of the instanses"
+}
+
+variable "key_name" {
+  description = "key name of the instance"
 }
