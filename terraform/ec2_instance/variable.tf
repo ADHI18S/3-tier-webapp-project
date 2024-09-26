5d311351d1bf02a7f3db52ee4a363a31769c7416
ADHI18s <sadhi684@gmail.com>
2024-09-26 10:43:11 +0530

    changes in variable.tf pushed

--- terraform/ec2_instance/variable.tf
+++ terraform/ec2_instance/variable.tf
@@ -6,6 +6,11 @@ variable "availability_zones" {
   description = "availability zone of subnet public"
 }
 
+variable "cidr_block" {
+  description = "cidr blocks of all the subnets"
+  default = ["10.0.0.0/16","10.0.24.0/24","10.0.26.0/24","0.0.0.0/0","0.0.0.0/0"]
+}
+
 variable "ami" {
   description = "ami for the 2 instanses"
 }
